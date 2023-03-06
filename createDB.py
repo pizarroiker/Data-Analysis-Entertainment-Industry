ef07519da50192b6277ddcbf5b6d67d39cebf41a
BraisCabo <braiscabofelpete@gmail.com>
2023-03-06 20:23:01 +0100

    Table creation fix

--- createDB.py
+++ createDB.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def createDataTable():
     con = sqlite3.connect("SI.db")
-    df = pd.read_csv("netflix_titles.csv",sep = ';', header=1)
+    df = pd.read_csv("netflix_titles.csv",sep = ';', header=0)
     df.to_sql("show", con, schema = None, if_exists= 'replace', index = False)
     con.commit()
 
