4a9e62b16652dedc5e29e97f4225630497809d2e
i.pizarro.2020 <pizarroiker5@gmail.com>
2023-03-07 11:13:32 +0100

    Number of Samples Fix

--- createDB.py
+++ createDB.py
@@ -10,11 +10,11 @@ def createDataTable():
 
 def num_samples():
     con = sqlite3.connect("SI.db")
-    frase = "SELECT COUNT(*) FROM show WHERE show_id IS NOT NULL AND type IS NOT NULL AND title IS NOT NULL AND " \
+    frase = "SELECT * FROM show WHERE show_id IS NOT NULL AND type IS NOT NULL AND title IS NOT NULL AND " \
             "director IS NOT NULL AND country NOT NULL AND date_added NOT NULL AND " \
             "release_year NOT NULL AND rating NOT NULL AND duration NOT NULL AND listed_in NOT NULL AND " \
-            "description NOT NULL AND cast NULL"
-    tam = pd.read_sql_query(frase,con)
+            "description NOT NULL and 'cast' NOT NULL"
+    tam = pd.read_sql_query(frase,con).shape[0]
     print("Numero de muestras sin missing values: " + str(tam))
 
 createDataTable()
