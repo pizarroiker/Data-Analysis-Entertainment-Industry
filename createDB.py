acab83cdf1e332708b39c1df30d9c5e95af4ecbb
i.pizarro.2020 <pizarroiker5@gmail.com>
2023-03-10 13:17:45 +0100

    Apartado 3

--- createDB.py
+++ createDB.py
@@ -1,5 +1,7 @@
 import math
+import re
 import sqlite3
+from statistics import mean
 
 import numpy as np
 import pandas as pd
@@ -56,6 +58,31 @@ def min_duracion():
     print("Mínima duración de una película: " + str(std_film)+ " minutos")
     print("Mínima duración de una serie: " + str(std_show) + " temporadas")
 
+def group_dataframe():
+    con = sqlite3.connect("SI.db")
+    frase = "SELECT  * FROM show WHERE type = 'Movie' AND CAST(duration as integer) >= 90 "
+    frase2 = "SELECT  *  FROM show WHERE type = 'Movie' AND CAST(duration as integer) < 90 "
+    frase3 = "SELECT  *  FROM show WHERE type = 'TV Show' AND CAST(duration as integer) > 2 "
+    frase4 = "SELECT * FROM show WHERE type = 'TV Show' AND CAST(duration as integer) <= 2 "
+    f1 = pd.read_sql_query(frase, con)
+    f2 = pd.read_sql_query(frase2, con)
+    f3 = pd.read_sql_query(frase3, con)
+    f4 = pd.read_sql_query(frase4, con)
+    f1['duration'] = f1['duration'].apply(lambda x: re.sub('[^0-9]','',x)).astype(int)
+    f2['duration'] = f2['duration'].apply(lambda x: re.sub('[^0-9]','', x)).astype(int)
+    f3['duration'] = f3['duration'].apply(lambda x: re.sub('[^0-9]','', x)).astype(int)
+    f4['duration'] = f4['duration'].apply(lambda x: re.sub('[^0-9]','', x)).astype(int)
+    return f1,f2,f3,f4
+
+def info_f(f):
+    print("Número de películas: "+str(f['duration'].shape[0]))
+    print("Número de missing values: "+ str(f.isnull().sum().sum()))
+    print("Mediana: "+ str(f['duration'].median()))
+    print("Media: " + str(round(f['duration'].mean(), 2)))
+    print("Varianza: " + str(round(f['duration'].var(), 2)))
+    print("Máximo: " + str(f['duration'].max()))
+    print("Mínimo: " + str(f['duration'].min()))
+
 createDataTable()
 print()
 print("------ APARTADO 2 ------")
@@ -66,4 +93,20 @@ max_duracion()
 min_duracion()
 print()
 print("------ APARTADO 3 ------")
-
+print()
+f1,f2,f3,f4 = group_dataframe()
+print("----- Películas que duran más de 90 minutos o 90 minutos ----")
+print()
+info_f(f1)
+print()
+print("----- Películas que duran menos de 90 minutos ----")
+print()
+info_f(f2)
+print()
+print("----- Series que duran más de 2 temporadas -----")
+print()
+info_f(f3)
+print()
+print("----- Series que duran 2 temporadas o menos -----")
+print()
+info_f(f4)
