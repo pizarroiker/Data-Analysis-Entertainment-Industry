c38d491ee89fc36110d47d4b79aa2e737d14a093
i.pizarro.2020 <pizarroiker5@gmail.com>
2023-04-26 11:01:11 +0200

    User data update

--- generarcsv.py
+++ generarcsv.py
@@ -2,6 +2,7 @@ import csv
 import random
 from datetime import datetime, timedelta
 
+countries = ["Spain", "Germany", "France", "Japan", "USA"]
 # Generar 1000 filas de datos aleatorios
 rows = []
 for i in range(1000):
@@ -14,11 +15,13 @@ for i in range(1000):
     # Generar una fecha de inicio de sesión aleatoria en los últimos 30 días
     last_login = datetime.now() - timedelta(days=random.randint(0, 30))
 
+    country = random.choice(countries)
+
     # Agregar la fila a la lista de filas
-    rows.append([id, name, last_login])
+    rows.append([id, name, last_login, country])
 
 # Escribir los datos en un archivo CSV
 with open("datos.csv", "w", newline="") as archivo_csv:
     writer = csv.writer(archivo_csv)
-    writer.writerow(["id", "Nombre", "Ultima Fecha de Inicio de Sesion"])
+    writer.writerow(["id", "Nombre", "Ultima Fecha de Inicio de Sesion","Pais"])
     writer.writerows(rows)
